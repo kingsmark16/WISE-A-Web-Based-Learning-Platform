c439fbe6d71b437298fc1ccf8b6591555dbf2c1d
KingsMark <kingsmark16@users.noreply.github.com>
2025-09-17 13:53:09 +0800

    Revert "Merge pull request #14 from kingsmark16/feature/videoUpload"
    This reverts commit 815374ddb6b5f6e14a04d6fe0f1c7e9535b0ec56, reversing
    changes made to e65295ff8b23fc178e581c5efaf435c2c42ea64a.

--- backend/src/lib/prisma.js
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const g = globalThis;
-export const prisma = g._prisma || new PrismaClient();
-if (process.env.NODE_ENV !== 'production') g._prisma = prisma;
-
-export default prisma;
